c244ab50548f29765b84f3e314e9ccfede43178a
Oscar Salvador Macias Tiscareno <oscar.macias@infotec.mx>
2016-08-19 13:34:48 -0500

    procfile and web app runner for heroku

---
+++ src/main/java/com/service/rest/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.service.rest;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6022114461790281582L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		resp.setContentType("application/hal+json;charset=UTF-8");
+		resp.getWriter().append("Hello World");
+	}
+}
